0721baefb5007bd5e5f477ecaef31661ee66c8fa
OMRONEMCSM <github-support-emc-sm-jp-j@omron.com>
2019-03-27 09:36:37 +0900

    dummy files for the arduino library format.
    - In test, the `extra` directory was not needed.

---
+++ src/dummy.h
@@ -0,0 +1,6 @@
+/**
+ * 2JCIE-EV01 Library: just provides example projects, this is dummy file.
+ *
+ * OMRON Sensor evaluation kit did not need a special library or trick.
+ * you can connect sensors with wire.h, spi.h and DigitalWrite/Read etc.
+ */
