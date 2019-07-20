9a0c68475cdc6aaf805950a543230477f9fd1755
OMRONEMCSM <github-support-emc-sm-jp-j@omron.com>
2019-07-20 10:12:27 +0900

    change source files for D6T sensor series.

--- examples/mmic-ar/mmic-ar.ino
+++
@@ -1,5 +0,0 @@
-/*
- * please use the example `InputSerialPlotter` from
- * `Examples for Arduino MKR WiFi 1010 -> I2S`
- */
-#error this is dummy file, please see above comment.
--- examples/mmic-ft/mmic-ft.ino
+++
@@ -1,6 +0,0 @@
-/*
- * please use the example `InputSerialPlotter` from
- * `https://github.com/maspetsberger/esp32-i2s-mems`
- */
-#error this is dummy file, please see above comment.
-
