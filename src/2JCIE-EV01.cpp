0b59195228799365d328f7e824160be156cf8f3d
OMRON Developers Hub <45954736+omron-devhub@users.noreply.github.com>
2026-09-01 16:27:01 +0900

    Add files via upload

---
+++ src/2JCIE-EV01.cpp
@@ -0,0 +1,6 @@
+/**
+ * 2JCIE-EV01 Library: just provides example projects, this is dummy file.
+ *
+ * OMRON Sensor evaluation kit did not need special libraries or tricks.
+ * you can connect sensors with wire.h, spi.h, DigitalWrite/Read and etc.
+ */
